bc90265ecd88a0078c26355f86469e6752194d84
Richard Neish <richard@neishfamily.org>
2021-09-15 12:56:20 +0100

    Initial tests from Toby/Stuart

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,5 +1,6 @@
 package org.xpdojo.bank;
 
+import org.assertj.core.api.AbstractIntegerAssert;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +9,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void startingBalanceIsZero() {
+        Account account = new Account();
+        assertThat(account.balance).isEqualTo(0);
+    }
+
+    @Test
+    public void depositAnAmountToIncreaseTheBalanceByTheSameAmount() {
+        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.balance).isEqualTo(100);
+    }
+
+    @Test
+    public void depositMultipleAmounts() {
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(300);
+        assertThat(account.balance).isEqualTo(400);
     }
 }
